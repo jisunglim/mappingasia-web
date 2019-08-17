d00ac42497194e0b5d7dfeaafd4125dae72bb56a
Jisung Lim <iejisung@gmail.com>
2019-08-17 22:14:18 +0900

    Updates

---
+++ precache-manifest.11bcb06516d6ecdb2e0695db30661316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d11c172c1d761e44c89e75b1165195",
+    "url": "/mappingasia-web/index.html"
+  },
+  {
+    "revision": "9073e7267a8a9809bcad",
+    "url": "/mappingasia-web/static/css/2.6639d161.chunk.css"
+  },
+  {
+    "revision": "6a1e0781e7c129fe702b",
+    "url": "/mappingasia-web/static/css/main.d8d65309.chunk.css"
+  },
+  {
+    "revision": "9073e7267a8a9809bcad",
+    "url": "/mappingasia-web/static/js/2.f2180ce5.chunk.js"
+  },
+  {
+    "revision": "6a1e0781e7c129fe702b",
+    "url": "/mappingasia-web/static/js/main.1d6fae1c.chunk.js"
+  },
+  {
+    "revision": "89948f082606e7308b1f",
+    "url": "/mappingasia-web/static/js/runtime~main.5755567b.js"
+  }
+]);
